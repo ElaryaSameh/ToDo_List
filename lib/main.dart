88087a75ac7fc16df412e6aef35390bcaf274293
Elarya sameh <elaryasameh@gmail.com>
2025-05-08 18:05:23 +0300

    todo

---
+++ lib/main.dart
@@ -0,0 +1,25 @@
+import '../Screans/addtasks.dart';
+import '../Screans/login.dart';
+import '../Screans/register.dart';
+import '../onbord.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home:
+          // AddTasks(),
+          OnBording(),
+      //  Register(),
+      // Login(),
+    );
+  }
+}
